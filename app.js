c4416138790778fda33b1b7a1bdf40da717d831b
Leo <leoquan09@gmail.com>
2026-05-18 17:43:02 -0700

    fixed

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const path = require('path');
-const fs = require('path');
+const path = require('fs');
 const app = express();
 
 app.use(express.static(__dirname));
